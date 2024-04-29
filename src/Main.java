205bb12d2afad5183a995eef22ddfbb10106e100
IlyasKmch <9897636381@mail.ru>
2024-04-29 12:36:32 +0300

    first

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int cost = 17_676;
+        int RubelsForMile = 20;
+        int miles;
+        miles = cost / RubelsForMile;
+
+        System.out.println(miles);
+    }
+
+}
